c562522942e5f72db465556ce3c5bbd51d943cb2
HirbodBehnam <chrome.hiri.angry@gmail.com>
2022-02-19 16:30:18 +0330

    Start the analysis progress in another thread Forbid the multi-convert scenario Added comments

--- main.py
+++ main.py
@@ -23,6 +23,11 @@ class ToChangeParams:
         return f"fps: {self.frame_rate} - res: {self.resolution} - v_codec: {self.v_codec} - a_codec: {self.a_codec}"
 
     def generate_parameters(self) -> list:
+        """
+        This method creates the ffmpeg arguments based on the components which must change
+        The list of arguments does not have the filenames in it
+        :return: List of flags of ffmpeg to run
+        """
         result = ['-y']  # overwrite
         # Check framerate
         if self.frame_rate:
@@ -44,27 +49,47 @@ class ToChangeParams:
         return result
 
 
-def time_to_seconds(string_time: str, prefix: str) -> int:
+def time_to_seconds(string_time: str, prefix: str = '') -> int:
+    """
+    Extracts a time of format HH:MM:SS from given string
+    :param string_time: The string to extract the time from
+    :param prefix: What prefix does the time have
+    :return: The number of seconds in the time
+    """
     length = re.findall(prefix + '([0-9]{2}:[0-9]{2}:[0-9]{2})', string_time)[-1].split(':')
     return int(length[0]) * 3600 + int(length[1]) * 60 + int(length[2])
 
 
 def format_seconds(seconds: int) -> str:
+    """
+    Returns a string in format of HH:MM:DD which represents the number of seconds given as the argument
+    :param seconds: The number of seconds to format
+    :return: A formatted string
+    """
     sec = seconds % 60
     minute = (seconds // 60) % 60
     hour = seconds // 3600
     return f"{hour:02}:{minute:02}:{sec:02}"
 
 
-def sizeof_fmt(num, suffix="B") -> str:
+def sizeof_fmt(num) -> str:
+    """
+    Converts a size in bytes to human-readable size
+    :param num: Number of bytes
+    :return: Human-readable size
+    """
     for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
         if abs(num) < 1024.0:
-            return f"{num:3.1f}{unit}{suffix}"
+            return f"{num:3.1f}{unit}B"
         num /= 1024.0
-    return f"{num:.1f}Yi{suffix}"
+    return f"{num:.1f}YiB"
 
 
 def select_input():
+    """
+    The main entry point of choose file click;
+    This function gets the input file, gets the output filename and does the conversion
+    """
     filetypes = (
         ('WebM', '*.webm'),
         ('All files', '*.*')
@@ -80,21 +105,38 @@ def select_input():
         title='Save',
         filetypes=filetypes
     )
+    if out_name == "":
+        return  # Do nothing
     if not out_name.endswith('.mp4'):
         out_name += '.mp4'
-    convert_file(filename, out_name)
+    threading.Thread(target=convert_file, args=(filename, out_name)).start()
 
 
 def convert_file(input_file: str, output_file: str):
+    # Change the progress bar
+    progress_bar.config(mode='indeterminate')
+    progress_bar.start(50)
+    progress_label.config(text='gathering file info...')
+    select_file_button.config(state='disabled')
+    # Get info
     to_change = analyze_file(input_file)
     command = ['ffmpeg', '-i', input_file]
     command.extend(to_change.generate_parameters())
     command.append(output_file)
     print('detected configs:', to_change)
-    threading.Thread(target=final_convert_spawn, args=(command, to_change.total_seconds)).start()
+    # Change the progress bar back and do the conversion
+    progress_bar.stop()
+    progress_bar.config(mode='determinate')
+    do_convert(command, to_change.total_seconds)
+    select_file_button.config(state='normal')
 
 
 def analyze_file(input_file: str) -> ToChangeParams:
+    """
+    This method analyzes the file given and finds out which parameters it must change
+    :param input_file: The input file location
+    :return: The parameters which must change
+    """
     probe = ffmpeg.probe(input_file)
     video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
     audio_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'audio'), None)
@@ -115,6 +157,11 @@ def analyze_file(input_file: str) -> ToChangeParams:
 
 
 def get_audio_bitrate_and_running_time(input_file: str) -> (int, int):
+    """
+    This function gets the audio bitrate and the running time of the file
+    :param input_file: The input file location
+    :return: A tuple of two ints. The first one is the bitrate in kb/s and the next one is the running time in seconds
+    """
     out = subprocess.run(['ffmpeg', '-i', input_file, '-c', 'copy', '-f', 'null', '-'],
                          stderr=subprocess.PIPE).stderr.decode('utf-8')
     audio_size = int(re.search('audio:([0-9]+)kB', out).groups()[0])
@@ -122,7 +169,13 @@ def get_audio_bitrate_and_running_time(input_file: str) -> (int, int):
     return audio_size * 8 // running_time, running_time
 
 
-def final_convert_spawn(args: list, total_seconds: int):
+def do_convert(args: list, total_seconds: int):
+    """
+    This function will start ffmpeg with given arguments and report the progress in UI
+    :param args: The arguments to run ffmpeg
+    :param total_seconds: The running time of movie
+    """
+    progress_bar['value'] = 0
     proc = subprocess.Popen(args, stdout=subprocess.PIPE)
     while True:
         line = proc.stdout.readline()
